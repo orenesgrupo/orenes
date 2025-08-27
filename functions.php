d8ff3911e13ab07055b8a9cf4070d00af6ebb134
orenesgrupo <samuel.cerezo@orenesgrupo.com>
2025-08-27 13:10:58 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -404,9 +404,9 @@ add_action('admin_enqueue_scripts', function() {
 
 	}
 
-	wp_enqueue_style('admin', get_template_directory_uri().'/css/admin.css', array(), uniqid());
+	wp_enqueue_style('admin', get_stylesheet_directory_uri().'/css/admin.css', array(), uniqid());
 
-	wp_enqueue_script('admin', get_template_directory_uri().'/js/admin.js', array('jquery'), uniqid(), true);
+	wp_enqueue_script('admin', get_stylesheet_directory_uri().'/js/admin.js', array('jquery'), uniqid(), true);
 
 });
 
